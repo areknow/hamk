7cb308a625fcbcdd92dcc4ae5fe0e4e748480dc4
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-25 15:35:03 -0500

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
+import * as firebase from 'firebase';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,4 +24,10 @@ import { environment } from 'src/environments/environment';
   providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(environment.firebase);
+    }
+  }
+}
