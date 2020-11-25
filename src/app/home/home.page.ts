d3e60175bb2d74cea8d034b840f085ddb3e54bb9
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-25 14:33:00 -0500

    cap and firestore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { cfaSignIn } from "capacitor-firebase-auth";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor() {}
-
+  ngOnInit() {
+    cfaSignIn("google.com").subscribe((user) => {
+      console.log(user.displayName);
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC3eh5p5cs5CWF5OPnu7VqZloSR6qfZbcQ',
+    authDomain: 'hamk-294619.firebaseapp.com',
+    databaseURL: 'https://hamk-294619.firebaseio.com',
+    projectId: 'hamk-294619',
+    storageBucket: 'hamk-294619.appspot.com',
+    messagingSenderId: '463813999142',
+    appId: '1:463813999142:web:a0c0644dcdb804012184e7',
+    measurementId: 'G-TT0HQYYGMP'
+  }
 };
 
 /*
